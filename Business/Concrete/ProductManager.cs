70ef67b50acc1bab6395cc0030079a52e21fdc0f
mhsnzkn <muhsinozkan12@gmail.com>
2021-03-03 03:02:38 +0200

    Business layer is done

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,50 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        private IProductDal productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            this.productDal = productDal;
+        }
+
+        public void Add(Product product)
+        {
+            productDal.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            productDal.Delete(product);
+        }
+
+        public Product GetById(int id)
+        {
+            return productDal.Get(a => a.ProductId == id);
+        }
+
+        public List<Product> GetList()
+        {
+            return productDal.GetList().ToList();
+        }
+
+        public List<Product> GetListByCategory(int categoryId)
+        {
+            return productDal.GetList(a => a.CategoryId == categoryId).ToList();
+        }
+
+        public void Update(Product product)
+        {
+            productDal.Update(product);
+        }
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/Contexts/NorthwindContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework.Contexts
+{
+    public class NorthwindContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\SQL2016;Database=Nortwind;Trusted_Connection=true");
+        }
+    }
+}
