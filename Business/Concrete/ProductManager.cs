e29c42fe6767602a83adc8d5045a18d742a9dd8f
mhsnzkn <muhsinozkan12@gmail.com>
2021-05-30 12:48:03 +0300

    ProductService updated according to Results

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using System;
@@ -17,34 +18,36 @@ namespace Business.Concrete
             this.productDal = productDal;
         }
 
-        public void Add(Product product)
+        public IDataResult<Product> GetById(int id)
+        {
+            return new SuccessDataResult<Product>(productDal.Get(a => a.ProductId == id));
+        }
+
+        public IDataResult<List<Product>> GetList()
+        {
+            return new SuccessDataResult<List<Product>>(productDal.GetList().ToList());
+        }
+
+        public IDataResult<List<Product>> GetListByCategory(int categoryId)
+        {
+            return new SuccessDataResult<List<Product>>(productDal.GetList(a => a.CategoryId == categoryId).ToList());
+        }
+
+        public IResult Add(Product product)
         {
             productDal.Add(product);
+            return new SuccessResult("Urun basariyla eklendi");
         }
 
-        public void Delete(Product product)
+        public IResult Delete(Product product)
         {
             productDal.Delete(product);
+            return new SuccessResult("Urun basariyla silindi");
         }
-
-        public Product GetById(int id)
-        {
-            return productDal.Get(a => a.ProductId == id);
-        }
-
-        public List<Product> GetList()
-        {
-            return productDal.GetList().ToList();
-        }
-
-        public List<Product> GetListByCategory(int categoryId)
-        {
-            return productDal.GetList(a => a.CategoryId == categoryId).ToList();
-        }
-
-        public void Update(Product product)
+        public IResult Update(Product product)
         {
             productDal.Update(product);
+            return new SuccessResult("Urun basariyla Guncellendi");
         }
     }
 }
