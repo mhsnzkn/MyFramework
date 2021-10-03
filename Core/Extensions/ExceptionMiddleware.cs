0914947165b3c06de9ba635af26eaa47e2402149
mhsnzkn <muhsinozkan12@gmail.com>
2021-10-03 20:53:20 +0100

    Exception Error Middleware added

---
+++ Core/Extensions/ExceptionMiddleware.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Extensions
+{
+    public class ExceptionMiddleware
+    {
+        private RequestDelegate next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await next(httpContext);
+            }
+            catch (Exception e)
+            {
+                await HandleExceptionAsync(httpContext, e);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext httpContext, Exception e)
+        {
+            httpContext.Response.ContentType = "application/json";
+            httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            return httpContext.Response.WriteAsync(new ErrorDetail
+            {
+                StatusCode = httpContext.Response.StatusCode,
+                Message = "Internal Server Error"
+            }.ToString());
+        }
+    }
+}
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -76,6 +76,8 @@ namespace WebAPI
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
             }
 
+            app.ConfigureCustomExceptionMiddleware();
+
             app.UseCors("AllowOrigin");
 
             app.UseHttpsRedirection();
